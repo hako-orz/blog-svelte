3cdcdc0661d2574869fa1cf04facb1ebad9f4da6
hako-orz <1349581851@qq.com>
2025-03-31 06:02:41 +0800

    Initial

---
+++ src/components/Nav.svelte
@@ -0,0 +1,48 @@
+<script lang="ts">
+	const items = [
+		{ title: 'Home', href: '/' },
+		{ title: 'About', href: '/about' }
+	];
+</script>
+
+<nav class="main-nav">
+	<div>Hako</div>
+	<div class="nav-items">
+		{#each items as { title, href }}
+			<a class="nav-link" {href}>{title}</a>
+		{/each}
+	</div>
+    <div>
+        switch
+    </div>
+</nav>
+
+<style>
+	.main-nav {
+        box-sizing: border-box;
+        width: 100%;
+        height: 60px;
+        padding: 0 40px;
+
+		display: flex;
+		align-items: center;
+		justify-content: space-between;
+
+        position: fixed;
+        top: 0;
+        left: 0;
+	}
+
+    .nav-items {
+        display: flex;
+        gap: 12px;
+    }
+
+    .nav-link {
+        font-weight: bold;
+    }
+
+    .nav-link:hover {
+        color: blue;
+    }
+</style>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+    import Nav from '../components/Nav.svelte';
+    import type { LayoutProps } from './$types';
+
+    let { children }: LayoutProps = $props();
+</script>
+
+<header>
+    <Nav />
+</header>
+<main class="container">
+    {@render children()}
+</main>
+<footer class="footer">
+    &copy; ave mujica
+</footer>
+
+<style>
+    .container {
+        flex: 1;
+
+        box-sizing: border-box;
+        padding-top: 60px;
+        width: 100%;
+        max-width: 960px;
+
+        margin: 0 auto;
+    }
+
+    .footer {
+        text-align: center;
+
+        padding: 40px 0;
+    }
+</style>
